002392a6c59c67e4b9c5c630cbd4ef3886b5d138
Eric Zhang <interstellord226@gmail.com>
2020-01-25 15:30:15 -0500

    I added stuff
    Guilt + Health bars and global vars

---
+++ scenes/GuiltBar.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public class GuiltBar : TextureProgress
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        var global = (Health)GetNode("/root/Global");
+        this.Value = global.guilt;
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
