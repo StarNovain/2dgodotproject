184c690690309c811b012d1564c07a517351e722
Eric Zhang <interstellord226@gmail.com>
2020-01-25 18:26:55 -0500

    qweqwe
    qwe

--- scripts/Bullet.cs
+++ scripts/Bullet.cs
@@ -8,14 +8,22 @@ public class Bullet : KinematicBody2D
     // private string b = "text";
 
     // Called when the node enters the scene tree for the first time.
+    void move() {
+
+    }
+    
     public override void _Ready()
     {
+        var CombatP = (Node2D) GetNode("CombatPlayer");
+        var pPos = CombatP.GetPosition();
+
+        this.Position.Set(0,0);
         
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
+//  //public override void _Process(float delta)
 //  {
-//      
+//
 //  }
 }
