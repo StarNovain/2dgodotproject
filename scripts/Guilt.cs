e651532d554099fbc7ee21c44920770ffb4b567b
Eric Zhang <interstellord226@gmail.com>
2020-01-25 15:44:52 -0500

    sdf

--- scripts/Guilt.cs
+++ scripts/Guilt.cs
@@ -1,18 +1,16 @@
 using Godot;
 using System;
 
-public class Global : Node
+public class Guilt : Node
 {
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
-    public int health {get; set;}
-    public int guilt {get; set;}
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        health = 100;
-        guilt = 100;
+        
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
