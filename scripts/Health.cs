a652aff42372466defa32a2fa807ff156a656b0d
Eric Zhang <interstellord226@gmail.com>
2020-01-25 14:51:59 -0500

    More stuff

--- scripts/Health.cs
+++ scripts/Health.cs
@@ -10,7 +10,7 @@ public class Health : Node
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        int Health = 100;
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
