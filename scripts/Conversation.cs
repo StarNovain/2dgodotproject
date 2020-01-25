b5b0bd2a73f30ea8a4ccb27ecb75537bf3fdd494
Luke Trenaman <trenamanluke@gmail.com>
2020-01-25 16:58:36 -0500

    work

--- scripts/Conversation.cs
+++ scripts/Conversation.cs
@@ -8,9 +8,15 @@ public class Conversation : Node2D
     // private string b = "text";
 
     // Called when the node enters the scene tree for the first time.
+    JSONParseResult obj;
     public override void _Ready()
     {
-        
+        GD.Print(System.Environment.CurrentDirectory + "\\scripts\\base.json");
+        string text = System.IO.File.ReadAllText(System.Environment.CurrentDirectory + "\\scripts\\base.json");
+        obj = JSON.Parse(text);
+        object dialogue = obj.Result;
+
+        //Directory.GetCurrentDirectory
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
